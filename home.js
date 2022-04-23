0e3a3381424be5bd5bffc414bbc30eff088ecc72
Narendra Singh Anjana <narendraanjana09@gmail.com>
2022-04-23 12:37:23 +0530

    changes

--- home.js
+++ home.js
@@ -36,12 +36,5 @@ function onSignIn(profile) {
         name_tv.innerHTML =profile.displayName;
         email_tv.innerHTML=profile.email;
         profile_img.src = profile.photoURL;
-    
-}
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
 }
 
